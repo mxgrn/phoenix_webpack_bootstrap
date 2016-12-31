aadb34d5f29470274c2cf403f80468b5795a3109
Max Gorin <gorinme@gmail.com>
2016-12-31 16:43:19 +0700

    Default Phoenix behavior, no Bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+// Mostly copied from https://gist.github.com/ecpplus/c2196381c26d02241bc060ce2a8601da
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+
+const config = {
+  devtool: "source-map",
+  entry: {
+    "app": ["./web/static/css/app.css", "./web/static/js/app.js"],
+  },
+
+  output: {
+    path: "./priv/static",
+    filename: "js/app.js"
+  },
+
+  resolve: {
+    modulesDirectories: [ "node_modules", __dirname + "/web/static/js" ]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['es2015']
+        }
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style", "css")
+      }
+    ]
+  },
+
+  devtool: ['cheap-module-source-map'],
+
+  plugins: [
+    new ExtractTextPlugin("css/app.css"),
+    new CopyWebpackPlugin([{ from: "./web/static/assets" }]),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+  ],
+
+  postcss: () => {
+    return [autoprefixer({
+      browsers: ['last 2 versions']
+    })];
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true,
+                warnings: false,
+            }
+        })
+    )
+} else {
+    config.devtool = "#cheap-module-source-map"
+}
+
+module.exports = config
